ce7b6fae5eb3758dbe5058a5bab0be03d6912637
ToyZzz <584638298@qq.com>
2017-11-13 20:51:24 +0800

    temp

---
+++ Client/UnityClient/KNClient/Assets/test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.Log("aaaaaaaaa");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
